0c59c2794da71d48e4183ab3153220bb9fa33697
graciolinetodevops <graciolineto.devops@gmail.com>
2025-11-10 16:10:07 -0400

    ci: adiciona pipeline simples de testes e sintaxe

---
+++ tests/test_produto.py
@@ -0,0 +1,16 @@
+from produto import Produto
+
+def test_venda_sucesso():
+    p = Produto("Mouse", 100, estoque=10)
+    assert p.vender(2)
+    assert p.estoque == 8
+
+def test_venda_insuficiente():
+    p = Produto("Mouse", 100, estoque=1)
+    assert not p.vender(2)
+    assert p.estoque == 1
+
+def test_repor_produto():
+    p = Produto("Teclado", 200, estoque=5)
+    p.repor(3)
+    assert p.estoque == 8
