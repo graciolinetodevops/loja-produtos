2054d1f6bd1a0373bad31bb52a22524e79e86f2e
graciolinetodevops <graciolineto.devops@gmail.com>
2025-11-10 16:23:44 -0400

    test: força falha proposital nos testes

--- tests/test_produto.py
+++ tests/test_produto.py
@@ -3,7 +3,7 @@ from produto import Produto
 def test_venda_sucesso():
     p = Produto("Mouse", 100, estoque=10)
     assert p.vender(2)
-    assert p.estoque == 8
+    assert p.estoque == 999
 
 def test_venda_insuficiente():
     p = Produto("Mouse", 50, estoque=1)
